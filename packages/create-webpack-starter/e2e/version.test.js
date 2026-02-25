d59c3ee2d80f30040c40726f42ce62b6d05c6f04
bagel <ponomarenko.pavel.web@gmail.com>
2026-02-25 19:57:03 +0200

    create-webpack-starter: update version test

--- packages/create-webpack-starter/e2e/version.test.js
+++ packages/create-webpack-starter/e2e/version.test.js
@@ -13,5 +13,25 @@ import {runCLI} from './helpers/run-cli.js';
     {expectedExitCode: 0}
   );
 
+  await runCLI(
+    ['version'],
+    {expectedExitCode: 1}
+  );
+
+  await runCLI(
+    ['Version'],
+    {expectedExitCode: 1}
+  );
+
+  await runCLI(
+    ['V'],
+    {expectedExitCode: 1}
+  )
+
+  await runCLI(
+    ['v'],
+    {expectedExitCode: 1}
+  )
+
   console.log('✅ version.test.js passed');
 })();
