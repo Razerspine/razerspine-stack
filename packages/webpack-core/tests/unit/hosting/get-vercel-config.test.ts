4521f25fce4ffd50baea8fe872a8a994bfa0348d
bagel <ponomarenko.pavel.web@gmail.com>
2026-03-18 18:50:30 +0200

    webpack-core: fix get-vercel-config.test.ts

--- packages/webpack-core/tests/unit/hosting/get-vercel-config.test.ts
+++ packages/webpack-core/tests/unit/hosting/get-vercel-config.test.ts
@@ -4,12 +4,15 @@ import {getVercelConfig} from '../../../src/hosting/get-vercel-config';
 describe('getVercelConfig', () => {
     it('should return valid JSON for SPA', () => {
         const result = JSON.parse(getVercelConfig('spa'));
-        expect(result.routes[0].dest).toBe('/index.html');
+        expect(result.rewrites).toBeDefined();
+        expect(result.rewrites[0].source).toBe('/(.*)');
+        expect(result.rewrites[0].destination).toBe('/index.html');
     });
 
-    it('should return valid JSON for MPA with filesystem handle', () => {
+    it('should return valid JSON for MPA', () => {
         const result = JSON.parse(getVercelConfig('mpa'));
-        expect(result.routes).toContainEqual({handle: 'filesystem'});
-        expect(result.routes[1].status).toBe(404);
+        expect(result.cleanUrls).toBe(true);
+        expect(result.trailingSlash).toBe(false);
+        expect(result.rewrites).toBeUndefined();
     });
 });
