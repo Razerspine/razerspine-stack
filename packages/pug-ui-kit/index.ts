4449fcb8d21a5e882b7bd260c734a32ec1d0503c
bagel <ponomarenko.pavel.web@gmail.com>
2026-03-22 18:09:07 +0200

    pug-ui-kit: rename components -> mixins, update package.json, fix index.ts

--- packages/pug-ui-kit/index.ts
+++ packages/pug-ui-kit/index.ts
@@ -2,7 +2,11 @@ import * as path from 'path';
 
 export const paths = {
     fonts: path.join(__dirname, '../fonts'),
-    scss: path.join(__dirname, '../src/styles/scss'),
-    less: path.join(__dirname, '../src/styles/less'),
-    pug: path.join(__dirname, '../src/pug')
+    scss: path.join(__dirname, './scss'),
+    less: path.join(__dirname, './less'),
+    mixins: path.join(__dirname, './pug/mixins')
 };
+
+export function getStylePath(type: 'scss' | 'less' = 'scss') {
+    return path.join(__dirname, `./${type}/ui.${type}`);
+}
