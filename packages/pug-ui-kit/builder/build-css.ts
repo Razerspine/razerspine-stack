50bf9792c838e187a868913201ef07fb7ab6838d
bagel <ponomarenko.pavel.web@gmail.com>
2026-03-22 12:42:50 +0200

    pug-ui-kit: rename entry files, update build-css.ts

--- packages/pug-ui-kit/builder/build-css.ts
+++ packages/pug-ui-kit/builder/build-css.ts
@@ -2,12 +2,12 @@ import {execSync} from 'child_process';
 
 export function buildCSS() {
     execSync(
-        'sass src/styles/scss/ui-kit.scss dist/css/ui.css',
-        { stdio: 'inherit' }
+        'sass src/styles/scss/ui.scss dist/css/ui.css',
+        {stdio: 'inherit'}
     );
 
     execSync(
         'postcss dist/css/ui.css --config postcss.config.js --output dist/css/ui.min.css',
-        { stdio: 'inherit' }
+        {stdio: 'inherit'}
     );
 }
